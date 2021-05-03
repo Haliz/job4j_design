4c6dbcb7a180f951e996b9a94b34a8c974b8b1aa
Haliz <foril2n1@rambler.ru>
2021-05-03 16:56:02 +0300

    Поиск файлов по критерию [#403831]

---
+++ src/main/java/ru/job4j/io/find/Finder.java
@@ -0,0 +1,73 @@
+package ru.job4j.io.find;
+
+import ru.job4j.io.SearchFiles;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.function.Predicate;
+
+public class Finder {
+    private static final Map<String, String> values = new HashMap<>();
+
+    public static void validate(String[] args) {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Incorrect arguments \n"
+                    + "Введите аргументы поиска, например: -d=c:/ -n=.txt -t=mask -o=log.txt \n"
+                    + "-d - директория, в которой начинать поиск.\n"
+                    + "-n - имя файла (полностью), маска.\n"
+                    + "-t - тип поиска: mask - искать по маске, name - по полному совпадение имени.\n"
+                    + "-o - результат записать в файл."
+            );
+        }
+        for (String elem : args) {
+            String[] splitElem = elem.substring(1).split("=");
+            if (splitElem.length != 2) {
+                throw new IllegalArgumentException(String.format("Incorrect argument -%s", splitElem));
+            }
+            values.put(splitElem[0], splitElem[1]);
+        }
+        String[] arrKeys = {"d", "n", "t", "o" };
+        for (String key : arrKeys) {
+            if (!values.containsKey(key)) {
+                throw new IllegalArgumentException(String.format("Incorrect argument -%s", key));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Finder.validate(args);
+        Path start = Paths.get(values.get("d"));
+        Predicate<Path> condition;
+        if (values.get("t").equals("mask")) {
+            condition = p -> p.toFile().getName().contains((values.get("n")));
+        } else if (values.get("t").equals("name")){
+            condition = p -> p.toFile().getName().equals((values.get("n")));
+        } else {
+            throw new IllegalArgumentException("Incorrect argument -t");
+        }
+        List<Path> found = search(start, condition);
+        writingToFile(found);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    public static void writingToFile(List<Path> paths) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(values.get("o")))) {
+            if (paths.isEmpty()) {
+                writer.write("File(s) NOT FOUND!");
+            }
+            for (Path path : paths) {
+                writer.write(path.toString());
+                writer.newLine();
+            }
+            System.out.println("DONE!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
