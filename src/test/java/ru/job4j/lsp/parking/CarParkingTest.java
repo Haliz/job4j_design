5d80338337c453a016fba0b32de913748895bc16
Haliz <foril2n1@rambler.ru>
2021-09-18 20:48:11 +0300

    2. Парковка машин (2. Реализация тестов.) Правка 2

--- src/test/java/ru/job4j/lsp/parking/CarParkingTest.java
+++ src/test/java/ru/job4j/lsp/parking/CarParkingTest.java
@@ -27,58 +27,38 @@ public class CarParkingTest {
         assertThat(parking.parking(truck), is(false));
     }
 
-    @Test
-    public void checkParkingController() {
-        Parking parking = new CarParking(1, 1);
-        ParkingController controller = new ParkingController(parking);
-        Vehicles car = new PassengerCar();
-        Vehicles truck = new Truck(2);
-        controller.toParking(car);
-        controller.toParking(truck);
-        CarParking expParking = (CarParking) parking;
-        assertThat(expParking.getOccupiedCarSpaces(), is(1));
-        assertThat(expParking.getOccupiedTruckSpaces(), is(1));
-    }
 
     @Test
     public void when3CarsTo2CarPlaceAnd1TruckPlace() {
         Parking parking = new CarParking(2, 1);
-        ParkingController controller = new ParkingController(parking);
         Vehicles car = new PassengerCar();
-        controller.toParking(car);
-        controller.toParking(car);
-        controller.toParking(car);
-        CarParking expParking = (CarParking) parking;
-        assertThat(expParking.getOccupiedCarSpaces(), is(2));
+        assertThat(parking.parking(car), is(true));
+        assertThat(parking.parking(car), is(true));
+        assertThat(parking.parking(car), is(false));
     }
 
     @Test
     public void whenTruckToCarPlases() {
-        Parking parking = new CarParking(6, 0);
-        ParkingController controller = new ParkingController(parking);
+        Parking parking = new CarParking(5, 0);
         Vehicles car = new PassengerCar();
         Vehicles truck = new Truck(3);
-        controller.toParking(car);
-        controller.toParking(car);
-        controller.toParking(truck);
-        CarParking expParking = (CarParking) parking;
-        assertThat(expParking.getOccupiedCarSpaces(), is(5));
+        assertThat(parking.parking(car), is(true));
+        assertThat(parking.parking(car), is(true));
+        assertThat(parking.parking(truck), is(true));
+        assertThat(parking.parking(car), is(false));
+        assertThat(parking.parking(truck), is(false));
 
     }
 
     @Test
     public void whenTruckNoPlacedOnTruckPlaces() {
         Parking parking = new CarParking(6, 1);
-        ParkingController controller = new ParkingController(parking);
-        Vehicles car = new PassengerCar();
         Vehicles truck1 = new Truck(3);
         Vehicles truck2 = new Truck(2);
-        controller.toParking(car);
-        controller.toParking(car);
-        controller.toParking(truck1);
-        controller.toParking(truck2);
-        CarParking expParking = (CarParking) parking;
-        assertThat(expParking.getOccupiedCarSpaces(), is(4));
-        assertThat(expParking.getOccupiedTruckSpaces(), is(1));
+        assertThat(parking.parking(truck1), is(true));
+        assertThat(parking.parking(truck2), is(true));
+        assertThat(parking.parking(truck2), is(true));
+        assertThat(parking.parking(truck1), is(false));
+        assertThat(parking.parking(truck2), is(true));
     }
 }
